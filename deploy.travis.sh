023117cf49d94c0f572e77cf3d2a91f20bf12bab
superzxc1 <30519633+superzxc1@users.noreply.github.com>
2019-11-06 22:25:39 +0800

    Create deploy.travis.sh

---
+++ deploy.travis.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'note.zxc66.site' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:superzxc1/note-book.git master:gh-pages
+
+# 如果使用 travis 持续集成
+git push -f https://${access_token}@github.com/superzxc1/note-book.git master:gh-pages
+
+cd -
